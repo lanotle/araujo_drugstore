89d71248cd67d8b9275444f011beded3aad9f3d6
elton <elton@itexto.com.br>
2024-04-08 09:46:12 -0300

    added some changes to my repo!

--- src/test/java/tests/PetTest.java
+++ src/test/java/tests/PetTest.java
@@ -1,55 +1,36 @@
 package tests;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.everyItem;
-
 import org.testng.annotations.Test;
+
 @Test
 public class PetTest extends SetupGenericBase {
-	
      
 	@Test
-    public void searchPetsStatusPending() {    
-      
+    public void searchPetsStatusPending() {      
        
-        given().
-            queryParam("status", "pending").
-        when().
-            get("/pet/findByStatus").
-        then().
-            assertThat().
-            statusCode(200).
-            body("status", everyItem(equalTo("pending")));
+        given().queryParam("status", "pending").
+        when().get("/pet/findByStatus").
+        then().assertThat().statusCode(200).body("status", everyItem(equalTo("pending")));
     }	
 	
     @Test
     public void petnotfound() {
-       
-    
-        given().
-            pathParam("petId", "9999").
-        when().
-            get("/pet/{petId}").
-        then().
-            assertThat().
-            statusCode(404);
-            
+    	
+        given().pathParam("petId", "9999").
+        when().get("/pet/{petId}").
+        then().assertThat().statusCode(404);            
     }
 	
 	@Test
 	public void updateaPet() {
 		
         String requestBody = "{\"id\": "+faker.number().digit()+",\"category\": {\"id\": 1,\"name\": \""+faker.name().firstName()+"\"},\"name\": \""+faker.name().firstName()+"\",\"photoUrls\": [\"string\"],\"tags\": [{\"id\": 1,\"name\": \""+faker.name().firstName()+"\"}],\"status\": \"available\"}";
-
-        given().
-            contentType("application/json").
-            body(requestBody).
-        when().
-            put("/pet").
-        then().
-            assertThat().
-            statusCode(200).
-            body("status", equalTo("available"))
-            .log().all();
+        
+        given().contentType("application/json").body(requestBody).
+        when().put("/pet").
+        then().assertThat().statusCode(200).body("status", equalTo("available")).log().all();
     }
 }
--- src/test/java/tests/StoreTest.java
+++ src/test/java/tests/StoreTest.java
@@ -2,10 +2,7 @@ package tests;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
 import org.testng.annotations.Test;
-
-
 
 @Test
 public class StoreTest  extends SetupGenericBase {
@@ -14,15 +11,17 @@ public class StoreTest  extends SetupGenericBase {
 	public void testPlaceanOrder() {
 		
 		String requestBody = incrementId(null);
-		given().contentType("application/json").body(requestBody).when().post("/store/order").then().assertThat()
-				.statusCode(200).body("status", equalTo("placed")).log().body();
-	};
-
-	@Test
-	public void testPlaceaninvalidOrder() {	
-		String requestBody = incrementId("AAA");
-		given().contentType("application/json").body(requestBody).when().post("/store/order").then().assertThat()
-				.statusCode(400).log().all();
+		given().contentType("application/json").body(requestBody).
+		when().post("/store/order").
+		then().assertThat().statusCode(200).body("status", equalTo("placed")).log().body();
 	}
 
+	@Test
+	public void testPlaceaninvalidOrder() {
+		
+		String requestBody = incrementId("AAA");
+		given().contentType("application/json").body(requestBody).
+		when().post("/store/order").
+		then().assertThat().statusCode(400).log().all();
+	}
 }
