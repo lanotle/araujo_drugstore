3c1340749ce786bfeaf94b4d25e14a9606262dfc
elton <elton@itexto.com.br>
2024-04-07 21:45:18 -0300

    Upando repositório com o desafio concluído

---
+++ src/test/java/tests/SetupGenericBase.java
@@ -0,0 +1,33 @@
+package tests;
+
+import static io.restassured.RestAssured.baseURI;
+
+import java.util.Locale;
+import java.util.Random;
+
+import org.testng.annotations.BeforeClass;
+
+import com.github.javafaker.Faker;
+
+public class SetupGenericBase {
+	 Faker faker; 
+
+	@BeforeClass
+	public void setUp() {
+		faker = new Faker(new Locale("pt-BR")); 
+		baseURI = "https://petstore.swagger.io/v2";
+	}
+	
+	public String incrementId(String id) {
+
+		Random gerador = new Random();
+		String newId = id != null ? id : String.valueOf(gerador.nextInt(1000));
+		
+		String requestBody = "{\"id\": " + newId
+				+ ",\"petId\": 1,\"quantity\": 1,\"shipDate\": \"2024-04-03T12:24:33.262Z\",\"status\": \"placed\",\"complete\": true}";
+		 
+		return requestBody;
+	}
+
+	
+}
---
+++ src/test/java/tests/StoreTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+
+
+@Test
+public class StoreTest  extends SetupGenericBase {
+	
+	@Test
+	public void testPlaceanOrder() {
+		
+		String requestBody = incrementId(null);
+		given().contentType("application/json").body(requestBody).when().post("/store/order").then().assertThat()
+				.statusCode(200).body("status", equalTo("placed")).log().body();
+	};
+
+	@Test
+	public void testPlaceaninvalidOrder() {	
+		String requestBody = incrementId("AAA");
+		given().contentType("application/json").body(requestBody).when().post("/store/order").then().assertThat()
+				.statusCode(400).log().all();
+	}
+
+}
